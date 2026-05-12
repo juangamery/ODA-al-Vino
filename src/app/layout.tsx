2b9a41e02b75a66fcce5780a2ce70b2da05f929f
Carlos Federico  Gunther <carlosfedericogunther@MacBook-Pro-de-Carlos.local>
2026-05-12 17:17:26 -0300

    feat: add Meta Pixel tracking (fbq init + PageView)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -158,6 +158,26 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
 
+        {/* Meta Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '5583753594990251');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
         {/* Preload critical fonts */}
         <link
           rel="preload"
@@ -192,6 +212,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=5583753594990251&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel (noscript) */}
 
         <LayoutClient>{children}</LayoutClient>
       </body>
